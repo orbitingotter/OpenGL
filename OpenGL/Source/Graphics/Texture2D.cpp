149eb4726de7f80246d8eb82f4127126350c2063
Chandan Priyadarshi <tesseractsaretasty@gmail.com>
2023-01-14 02:00:46 +0530

    added anistropic filtering

--- OpenGL/Source/Graphics/Texture2D.cpp
+++ OpenGL/Source/Graphics/Texture2D.cpp
@@ -18,11 +18,11 @@ Texture2D::Texture2D(const std::string& filePath, const std::string& type)
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY, 16);
 
 
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, mWidth, mHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, mImageBuffer);
 	glGenerateMipmap(GL_TEXTURE_2D);
-
 
 	// unbind to later bind specific slot
 	Unbind();
